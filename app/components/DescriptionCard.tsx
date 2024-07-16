b8ae07494a062dcd448f847418bd06deb8771887
marccgrau <marcgrau@outlook.com>
2024-07-16 10:33:43 +0200

    make starting instructions more visible

--- app/components/DescriptionCard.tsx
+++ app/components/DescriptionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DescriptionCard: React.FC = () => {
   return (
-    <div className="mb-4 rounded-lg bg-blue-100 p-6 shadow-md">
+    <div className="mb-8 rounded-lg bg-blue-100 p-6 shadow-md">
       <h2 className="mb-2 text-lg font-bold">Welcome to the Experiment!</h2>
       <p className="text-gray-700">
         In this experiment, we will conduct a conversation to gather information for calculating a
@@ -15,7 +15,7 @@ const DescriptionCard: React.FC = () => {
         will be recorded to fill out a form. The conversation should last about 5 minutes. Please
         make sure you have a stable internet connection and a quiet environment.
       </p>
-      <p className="mt-2 text-gray-700">
+      <p className="mt-2 font-semibold text-gray-700">
         Please start by saying that you are ready for the conversation, make sure to press and hold
         the button while speaking to record the audio.
       </p>
